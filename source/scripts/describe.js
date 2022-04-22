4e5b2e0040e24cd0bd7b75f88242addb6a61878b
ChristianMar <christianlecce@gmail.com>
2022-04-22 11:30:18 +0200

    fix

--- source/scripts/describe.js
+++ source/scripts/describe.js
@@ -4,8 +4,10 @@ module.exports = () => {
   return new Promise((resolve, reject) => {
     return exec('git describe', (error, stdout, stderr) => {
       if (error) {
+        console.log('error', error);
         reject(error);
       }
+      console.log('stdout', stdout);
       resolve(stdout.replace(/\n/g, ''));
     });
   });
